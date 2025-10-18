0c6c20aecb7aafc1a538cfd08001fdf48e9080c2
pranav718 <raypranav718@gmail.com>
2025-10-18 23:23:29 +0530

    adding x auth

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,6 @@
 import { convexAuth } from "@convex-dev/auth/server";
-import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
+import Twitter from "@auth/core/providers/twitter";
 import { Password } from "@convex-dev/auth/providers/Password";
 
 export const { auth, signIn, signOut, store } = convexAuth({
@@ -8,6 +8,10 @@ export const { auth, signIn, signOut, store } = convexAuth({
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    Twitter({
+      clientId: process.env.AUTH_TWITTER_ID,
+      clientSecret: process.env.AUTH_TWITTER_SECRET,
     }),
     Password,
   ],
