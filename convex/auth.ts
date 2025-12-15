fc64c3e025ee76a3391430ce4f660029830e55cb
pranav718 <raypranav718@gmail.com>
2025-12-15 19:38:02 +0530

    more thanks

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,8 +1,8 @@
-import { convexAuth } from "@convex-dev/auth/server";
 import Google from "@auth/core/providers/google";
 import Twitter from "@auth/core/providers/twitter";
-import { Password } from "@convex-dev/auth/providers/Password";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
+import { Password } from "@convex-dev/auth/providers/Password";
+import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store } = convexAuth({
   providers: [
@@ -19,12 +19,22 @@ export const { auth, signIn, signOut, store } = convexAuth({
         return {
           id: data?.id,
           name: data?.name ?? username,
-          email: `${username}@x.local`, 
+          email: `${username}@x.local`,
           image: data?.profile_image_url?.replace("_normal", "") ?? undefined,
         };
       },
     }),
-    Password,
+    Password({
+      profile(params) {
+        const profile: { email: string; name?: string } = {
+          email: params.email as string,
+        };
+        if (params.name) {
+          profile.name = params.name as string;
+        }
+        return profile;
+      },
+    }),
     Anonymous,
   ],
 });
